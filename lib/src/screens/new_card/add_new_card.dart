c0e89304f9c7f02d1adebbe352940349f5118a2b
kyouyagenki <rebertas.g@ezweb.ne.jp>
2022-11-12 21:22:13 +0900

    delete card scan

--- lib/src/screens/new_card/add_new_card.dart
+++ lib/src/screens/new_card/add_new_card.dart
@@ -1,4 +1,3 @@
-import 'package:credit_card_scanner/credit_card_scanner.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
@@ -25,8 +24,6 @@ class AddNewCardForm extends StatefulWidget {
     this.numberFormSuffixIcon,
     this.cvvFormPrefixIcon,
     this.expiryDateFormPrefixIcon,
-    this.scanButtonIcon,
-    this.enableScanButton = true,
   }) : super(key: key);
 
   final Function(PaymentCard) addCard;
@@ -42,9 +39,7 @@ class AddNewCardForm extends StatefulWidget {
   final Widget? numberFormSuffixIcon;
   final Widget? cvvFormPrefixIcon;
   final Widget? expiryDateFormPrefixIcon;
-  final Widget? scanButtonIcon;
   final GlobalKey<FormState> formKey;
-  final bool enableScanButton;
 
   @override
   State<AddNewCardForm> createState() => _AddNewCardFormState();
@@ -197,22 +192,6 @@ class _AddNewCardFormState extends State<AddNewCardForm> {
             ),
           ),
           const Spacer(flex: 2),
-          widget.enableScanButton
-              ? OutlinedButton.icon(
-                  onPressed: widget.scan ?? scanCard,
-                  icon: widget.scanButtonIcon ??
-                      SvgPicture.asset("assets/icons/scan.svg"),
-                  label: Text(widget.scanCardButtonText ?? "Scan card"),
-                  style: widget.scanCardButtonStyle ??
-                      ElevatedButton.styleFrom(
-                        backgroundColor: primaryColor,
-                        minimumSize: const Size(double.infinity, 56),
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(12)),
-                        ),
-                      ),
-                )
-              : const SizedBox.shrink(),
           Padding(
             padding: const EdgeInsets.only(top: defaultPadding),
             child: ElevatedButton(
@@ -236,33 +215,6 @@ class _AddNewCardFormState extends State<AddNewCardForm> {
           )
         ],
       ),
-    );
-  }
-
-  void scanCard() async {
-    final card = await CardScanner.scanCard();
-
-    final cardNumber = card!.cardNumber;
-    var cardNumValue = TextEditingValue(text: cardNumber);
-    cardNumValue = CardNumberInputFormatter().formatEditUpdate(
-      cardNumValue,
-      cardNumValue,
-    );
-    var expiryDateValue = TextEditingValue(text: card.expiryDate);
-    // expiryDateValue = CardMonthInputFormatter().formatEditUpdate(
-    //   expiryDateValue,
-    //   expiryDateValue,
-    // );
-
-    cardNumberController.value = cardNumValue;
-    expiryDateController.value = expiryDateValue;
-    _getCardTypeFromNumber();
-    final expiryDate = card.expiryDate.split('/');
-    _paymentCard = PaymentCard(
-      name: card.cardHolderName,
-      number: card.cardNumber,
-      month: int.parse(expiryDate[0]),
-      year: int.parse(expiryDate[1]),
     );
   }
 }
